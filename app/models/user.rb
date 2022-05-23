d184fc04cda507d015d08e86c9f3c76e6cedbea6
FMY <n.fmy.0526@icloud.com>
2022-05-23 23:58:05 +0900

    userモデルとfoodモデルを関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :food, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end

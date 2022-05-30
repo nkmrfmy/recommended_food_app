736507c1308ab1de2ed2c8d5edde488a66db637b
FMY <n.fmy.0526@icloud.com>
2022-05-30 22:51:22 +0900

    CRUD機能を実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,22 +1,46 @@
+# frozen_string_literal: true
+
 class FoodsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
+
   def index
+    @foods = Food.includes(:user).order(:created_at)
   end
 
   def new
+    @food = Food.new
   end
 
   def create
+    food = current_user.foods.create!(food_params)
+    redirect_to food
   end
 
   def show
+    @food = Food.find(params[:id])
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @food.update!(food_params)
+    redirect_to @food
   end
 
   def destroy
+    food = Food.find(params[:id])
+    food.destroy!
+    redirect_to root_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :comment)
+  end
+
+  def set_post
+    @food = current_user.foods.find_by(id: params[:id])
+    redirect_to root_path, alert: '権限がありません' if @food.nil?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  has_many :food, dependent: :destroy
+  has_many :foods, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end

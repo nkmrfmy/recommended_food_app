c0b51ca73e1ea42519bf73ff59968e5357963d41
FMY <n.fmy.0526@icloud.com>
2022-06-14 00:53:00 +0900

    いいね機能用のルーティング,コントローラーを作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,14 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  # food.liked_usersでfoodをいいね！しているユーザーの一覧を取得できる
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
 
   mount_uploader :image, ImageUploader
+
+  # foodをuserがいいねしているときはtrue,していないときはfalse
+  def liked_by?(_user)
+    likes.exists?(user_id: user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :likes, dependent: :destroy
+  # user.liked_foodsでユーザーがいいね！している料理の一覧を取得できる
+  has_many :liked_foods, through: :likes, source: :food
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end
